5f4b1eb44379767090ccda959b6fadb2c7050f4e
kamil <k.wolski@finave.nl>
2021-12-06 21:18:00 +0100

    On word click, dynamic url is being created and user is being forwarded

--- src/WordCloud.js
+++ src/WordCloud.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { select } from "d3-selection";
 import "tippy.js/animations/scale.css";
@@ -6,282 +6,284 @@ import "tippy.js/dist/tippy.css";
 import "d3-transition";
 import ReactWordcloud from "react-wordcloud";
 
-const callbacks = {
-  getWordTooltip: (word) =>
-    `The word "${word.text}" appears ${word.value} times.`,
-  onWordClick: getCallback("onWordClick"),
-  onWordMouseOut: getCallback("onWordMouseOut"),
-  onWordMouseOver: getCallback("onWordMouseOver"),
-};
 
-const words = [
-  {
-    text: "told",
-    value: 40,
-  },
-  {
-    text: "mistake",
-    value: 11,
-  },
-  {
-    text: "thought",
-    value: 16,
-  },
-  {
-    text: "bad",
-    value: 17,
-  },
-  {
-    text: "told",
-    value: 40,
-  },
-  {
-    text: "mistake",
-    value: 11,
-  },
-  {
-    text: "thought",
-    value: 16,
-  },
-  {
-    text: "bad",
-    value: 17,
-  },
-  {
-    text: "told",
-    value: 40,
-  },
-  {
-    text: "mistake",
-    value: 11,
-  },
-  {
-    text: "thought",
-    value: 16,
-  },
-  {
-    text: "bad",
-    value: 17,
-  },
-  {
-    text: "told",
-    value: 40,
-  },
-  {
-    text: "mistake",
-    value: 11,
-  },
-  {
-    text: "thought",
-    value: 16,
-  },
-  {
-    text: "bad",
-    value: 17,
-  },
-  {
-    text: "told",
-    value: 40,
-  },
-  {
-    text: "mistake",
-    value: 11,
-  },
-  {
-    text: "thought",
-    value: 16,
-  },
-  {
-    text: "bad",
-    value: 17,
-  },
-  {
-    text: "told",
-    value: 40,
-  },
-  {
-    text: "mistake",
-    value: 11,
-  },
-  {
-    text: "thought",
-    value: 16,
-  },
-  {
-    text: "bad",
-    value: 17,
-  },
-  {
-    text: "told",
-    value: 40,
-  },
-  {
-    text: "mistake",
-    value: 11,
-  },
-  {
-    text: "thought",
-    value: 16,
-  },
-  {
-    text: "bad",
-    value: 17,
-  },
-  {
-    text: "told",
-    value: 40,
-  },
-  {
-    text: "mistake",
-    value: 11,
-  },
-  {
-    text: "thought",
-    value: 16,
-  },
-  {
-    text: "bad",
-    value: 17,
-  },
-  {
-    text: "told",
-    value: 40,
-  },
-  {
-    text: "mistake",
-    value: 11,
-  },
-  {
-    text: "thought",
-    value: 16,
-  },
-  {
-    text: "bad",
-    value: 17,
-  },
-  {
-    text: "told",
-    value: 40,
-  },
-  {
-    text: "mistake",
-    value: 11,
-  },
-  {
-    text: "thought",
-    value: 16,
-  },
-  {
-    text: "bad",
-    value: 17,
-  },
-  {
-    text: "told",
-    value: 40,
-  },
-  {
-    text: "mistake",
-    value: 11,
-  },
-  {
-    text: "thought",
-    value: 16,
-  },
-  {
-    text: "bad",
-    value: 17,
-  },
-  {
-    text: "told",
-    value: 40,
-  },
-  {
-    text: "mistake",
-    value: 11,
-  },
-  {
-    text: "thought",
-    value: 16,
-  },
-  {
-    text: "bad",
-    value: 17,
-  },
-  {
-    text: "told",
-    value: 40,
-  },
-  {
-    text: "mistake",
-    value: 11,
-  },
-  {
-    text: "thought",
-    value: 16,
-  },
-  {
-    text: "bad",
-    value: 17,
-  },
 
-  {
-    text: "told",
-    value: 40,
-  },
-  {
-    text: "mistake",
-    value: 11,
-  },
-  {
-    text: "thought",
-    value: 16,
-  },
-  {
-    text: "bad",
-    value: 17,
-  },
 
-  {
-    text: "told",
-    value: 40,
-  },
-  {
-    text: "mistake",
-    value: 11,
-  },
-  {
-    text: "thought",
-    value: 16,
-  },
-  {
-    text: "bad",
-    value: 17,
-  },
-];
-
-function getCallback(callback) {
-  return function (word, event) {
-    const isActive = callback !== "onWordMouseOut";
-    const element = event.target;
-    const text = select(element);
-    text
-      .on("click", () => {
-        if (isActive) {
-          <Link to="/subject/?apple" />;
-        }
-      })
-      .transition()
-      .attr("background", "white")
-      .attr("font-size", isActive ? "150%" : "100%")
-      .attr("text-decoration", isActive ? "underline" : "none");
-  };
-}
 
 export default function Wordcloud() {
+  const words = [
+    {
+      text: "told",
+      value: 40,
+    },
+    {
+      text: "mistake",
+      value: 11,
+    },
+    {
+      text: "thought",
+      value: 16,
+    },
+    {
+      text: "bad",
+      value: 17,
+    },
+    {
+      text: "told",
+      value: 40,
+    },
+    {
+      text: "mistake",
+      value: 11,
+    },
+    {
+      text: "thought",
+      value: 16,
+    },
+    {
+      text: "bad",
+      value: 17,
+    },
+    {
+      text: "told",
+      value: 40,
+    },
+    {
+      text: "mistake",
+      value: 11,
+    },
+    {
+      text: "thought",
+      value: 16,
+    },
+    {
+      text: "bad",
+      value: 17,
+    },
+    {
+      text: "told",
+      value: 40,
+    },
+    {
+      text: "mistake",
+      value: 11,
+    },
+    {
+      text: "thought",
+      value: 16,
+    },
+    {
+      text: "bad",
+      value: 17,
+    },
+    {
+      text: "told",
+      value: 40,
+    },
+    {
+      text: "mistake",
+      value: 11,
+    },
+    {
+      text: "thought",
+      value: 16,
+    },
+    {
+      text: "bad",
+      value: 17,
+    },
+    {
+      text: "told",
+      value: 40,
+    },
+    {
+      text: "mistake",
+      value: 11,
+    },
+    {
+      text: "thought",
+      value: 16,
+    },
+    {
+      text: "bad",
+      value: 17,
+    },
+    {
+      text: "told",
+      value: 40,
+    },
+    {
+      text: "mistake",
+      value: 11,
+    },
+    {
+      text: "thought",
+      value: 16,
+    },
+    {
+      text: "bad",
+      value: 17,
+    },
+    {
+      text: "told",
+      value: 40,
+    },
+    {
+      text: "mistake",
+      value: 11,
+    },
+    {
+      text: "thought",
+      value: 16,
+    },
+    {
+      text: "bad",
+      value: 17,
+    },
+    {
+      text: "told",
+      value: 40,
+    },
+    {
+      text: "mistake",
+      value: 11,
+    },
+    {
+      text: "thought",
+      value: 16,
+    },
+    {
+      text: "bad",
+      value: 17,
+    },
+    {
+      text: "told",
+      value: 40,
+    },
+    {
+      text: "mistake",
+      value: 11,
+    },
+    {
+      text: "thought",
+      value: 16,
+    },
+    {
+      text: "bad",
+      value: 17,
+    },
+    {
+      text: "told",
+      value: 40,
+    },
+    {
+      text: "mistake",
+      value: 11,
+    },
+    {
+      text: "thought",
+      value: 16,
+    },
+    {
+      text: "bad",
+      value: 17,
+    },
+    {
+      text: "told",
+      value: 40,
+    },
+    {
+      text: "mistake",
+      value: 11,
+    },
+    {
+      text: "thought",
+      value: 16,
+    },
+    {
+      text: "bad",
+      value: 17,
+    },
+    {
+      text: "told",
+      value: 40,
+    },
+    {
+      text: "mistake",
+      value: 11,
+    },
+    {
+      text: "thought",
+      value: 16,
+    },
+    {
+      text: "bad",
+      value: 17,
+    },
+  
+    {
+      text: "told",
+      value: 40,
+    },
+    {
+      text: "mistake",
+      value: 11,
+    },
+    {
+      text: "thought",
+      value: 16,
+    },
+    {
+      text: "bad",
+      value: 17,
+    },
+  
+    {
+      text: "told",
+      value: 40,
+    },
+    {
+      text: "mistake",
+      value: 11,
+    },
+    {
+      text: "thought",
+      value: 16,
+    },
+    {
+      text: "bad",
+      value: 17,
+    },
+  ];
+  const [wordsState, setWordsState] = useState('')
+let wordski = '';
+  const callbacks = {
+    onWordClick: getCallback("onWordClick"),
+  };
+  function getCallback(callbacks) {
+    return function (word, event) {
+      const isActive = callbacks !== "onWordClick";
+      const element = event.target;
+      const text = select(element);
+      console.log(word.text);
+      wordski = word.text;
+      setWordsState(wordski);
+      text
+        .on("click", () => {
+          if (isActive) {
+            <Link to={`/subject/${wordski}`}>{wordski}</Link>;
+          }
+        })
+        .transition()
+    };
+  }
+
+   
   return (
     <div>
-      <Link to="/subject/stack overflow">Apple</Link>;
+      <Link to={`/subject/${wordsState}`}>      
       <ReactWordcloud callbacks={callbacks} words={words} />
+      </Link>;
     </div>
   );
 }
